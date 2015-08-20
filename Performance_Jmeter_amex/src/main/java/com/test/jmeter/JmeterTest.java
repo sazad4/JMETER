336b7585d1a4ce7c0a461db66d13e94929d814e3
Shahriar Azad <Shahriar.Azad@aexp.com>
2015-08-20 15:47:15 -0400

    update
    update

---
+++ Performance_Jmeter_amex/src/main/java/com/test/jmeter/JmeterTest.java
@@ -0,0 +1,20 @@
+package com.test.jmeter;
+/**
+ * Sample Jmeter Test
+ *
+ */
+public class JmeterTest 
+{
+	
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hello Jmeter");
+		
+		
+		
+		
+	}
+
+}
