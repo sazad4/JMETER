01b14a09d151eb8810c0fce41410e592ada8be86
Shahriar Azad <Shahriar.Azad@aexp.com>
2015-08-25 17:14:43 -0400

    ss
    ss

---
+++ Sample_Project/src/main/java/com/test/Sample/SampleTest.java
@@ -0,0 +1,13 @@
+package com.test.Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class SampleTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "ID_GEN" );
+    }
+}
